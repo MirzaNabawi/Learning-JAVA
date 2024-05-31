ca4633536ba5a2ee1e1438a9159ebcc4aa095df9
Mirza Nabawi <119789438+MirzaNabawi@users.noreply.github.com>
2024-05-31 23:08:20 +0600

    Add files via upload

---
+++ Private Inner Class.java
@@ -0,0 +1,15 @@
+class OuterClass {
+  int x = 111;
+
+  class InnerClass {
+    int y = 9;
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    OuterClass myOuter = new OuterClass();
+    OuterClass.InnerClass myInner = myOuter.new InnerClass();
+    System.out.println(myInner.y + myOuter.x);
+  }
+}
